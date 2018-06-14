303e3afbfba0e50b3833d65e7c2009e3e4e8945b
ricarde <juansebastian.hormaza@endava.com>
2018-06-14 15:17:20 -0500

    Change the gulp and worker file

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,7 @@ gulp.task('generate-sw', function () {
   var swOptions = {
     staticFileGlobs: [
       './index.html',
-      // './mws-pwa/images/*.{png,svg,gif,jpg}',
-      '../mws-pwa/images/*.{png,svg,gif,jpg}',
+      './images/*.{png,svg,gif,jpg}',
       './scripts/*.js',
       './styles/*.css'
     ],
