16fb6a21beb7bf8b575993383fb7c8683ce3036e
ricarde <juansebastian.hormaza@endava.com>
2018-06-14 14:18:39 -0500

    Add the manifest and gulp file.

--- scripts/app.js
+++ scripts/app.js
@@ -148,6 +148,8 @@
     if (dateElem.getAttribute('data-dt') >= data.currently.time) {
       return;
     }
+    dateElem.setAttribute('data-dt', data.currently.time);
+    dateElem.textContent = new Date(data.currently.time * 1000);
     card.querySelector('.description').textContent = data.currently.summary;
     card.querySelector('.date').textContent =
       new Date(data.currently.time * 1000);
